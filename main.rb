f5b93e7ba9024136bdb264df0536d6c13f80182c
Alexander Adam <alexander.adam@vade.io>
2019-12-31 09:59:35 +0100

    add progressbar

--- main.rb
+++ main.rb
@@ -4,73 +4,62 @@
 require 'fileutils'
 require 'json'
 require 'mechanize'
+require 'progress_bar'
+
+DOWNLOAD_PATH = './audiobooks'.freeze
 
 def create_folder(directory)
   FileUtils.mkdir_p(directory) unless File.directory?(directory)
 end # create_folder
 
-def requests
+def client
   @browser ||= Mechanize.new
 end
 
-def get_all_audiobooks
-  audiobooks = []
-
-  url = 'https://iceportal.de/api1/rs/page/hoerbuecher'
-  response = requests.get(url)
+def audiobooks
+  response = client.get('https://iceportal.de/api1/rs/page/hoerbuecher')
 
   # extract titles
   json_data = JSON.parse(response.body)
-  items = json_data['teaserGroup']['items']
 
-  items.each do |item|
-    name = item['navigation']['href']
-    audiobooks.append(name)
+  json_data['teaserGroup']['items'].map do |item|
+    item['navigation']['href']
   end
-
-  audiobooks
 end # get_all_audiobooks
 
 def download_audiobook(path)
   title = path.split('/').last
-  puts("Downloading audiobook: #{title}")
+  puts("\nDownloading audiobook: #{title}")
 
-  url = "https://iceportal.de/api1/rs/page/hoerbuecher/#{title}"
-  chapter_response = requests.get(url)
+  chapter_response = client.get("https://iceportal.de/api1/rs/page/hoerbuecher/#{title}")
 
   # extract chapters
   json_data = JSON.parse(chapter_response.body)
   playlist = json_data['files']
 
-  # extract download_path for each chapter
-  download_path = []
-  playlist.each do |chapter|
-    chapter_path = chapter['path']
+  # extract download_paths for each chapter
+  download_paths = playlist.map do |chapter|
+    url = "https://iceportal.de/api1/rs/audiobooks/path#{chapter['path']}"
+    response_download_path = client.get(url)
 
-    url = "https://iceportal.de/api1/rs/audiobooks/path#{chapter_path}"
-    response_download_path = requests.get(url)
-
-    path = JSON.parse(response_download_path.body)['path']
-    download_path.append(path)
+    JSON.parse(response_download_path.body)['path']
   end
 
-  create_folder("./audiobooks/#{title}")
+  create_folder("./#{DOWNLOAD_PATH}/#{title}")
+  progress_bar = ProgressBar.new(download_paths.count)
 
   # download each track
-  download_path.each_with_index do |track, counter|
-    puts("#{counter + 1}/#{download_path.length}")
+  download_paths.each_with_index do |track, counter|
+    progress_bar.increment!
 
     url = "https://iceportal.de#{track}"
 
-    save_path = "audiobooks/#{title}/#{title}_#{counter + 1}.mp3"
-    requests.download(url, save_path)
+    save_path = File.join(DOWNLOAD_PATH, title, "#{title}_#{counter + 1}.mp3")
+    client.download(url, save_path)
   end
 end # download_audiobook
 
 # MAIN
-# extract all audiobooks
-audiobooks = get_all_audiobooks
-create_folder('./audiobooks')
 
 # download all audibooks
 audiobooks.each do |book|
